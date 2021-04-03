114ab019aff41306870e2cde6cede5c0ec95fa96
hassan <m.h.raxa1@gmail.com>
2021-04-03 14:54:54 +0500

    login screen redesign

--- Queensman-Client-master/src/Login/LoginScreen.js
+++ Queensman-Client-master/src/Login/LoginScreen.js
@@ -1,7 +1,3 @@
-/* eslint-disable react/no-access-state-in-setstate */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable no-shadow */
-/* eslint-disable no-console */
 import React from "react";
 import {
   StyleSheet,
@@ -22,26 +18,23 @@ import NetInfo from "@react-native-community/netinfo";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { Icon } from "native-base";
-import * as Animatable from "react-native-animatable";
 import axios from "axios";
 import * as LocalAuthentication from "expo-local-authentication";
 import Constants from "expo-constants";
 
-import Toast from "react-native-whc-toast";
 import { auth } from "../utils/nhost";
 import { endpoint } from "../constants/Endpoint";
 
 const deviceWidth = Dimensions.get("window").width;
 const deviceHeight = Dimensions.get("window").height;
 
-const TextBoxH = deviceHeight / 15.6;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+    paddingTop: "30%",
     paddingHorizontal: "12%",
   },
   gradiantStyle: {
@@ -99,8 +92,8 @@ export default class LoginScreen extends React.Component {
 
   async componentDidMount() {
     console.log("here LoginScreen");
-    this.checkDeviceForHardware();
-    this.checkForFingerprints();
+    // this.checkDeviceForHardware();
+    // this.checkForFingerprints();
   }
 
   toggleSwitch = () => {
@@ -150,7 +143,7 @@ export default class LoginScreen extends React.Component {
       // alert("Fingerprint not recognized!")
       // this.customToast.show("Fingerprint not recognized!");
       if (Constants.platform.android) {
-        this.scanFingerprint();
+        // this.scanFingerprint();
       }
     }
   };
@@ -173,7 +166,7 @@ export default class LoginScreen extends React.Component {
         this.setState({
           passwordcheck: result.data.server_responce.password,
         });
-        console.log(this.state.passwordcheck);
+        console.log({ passowrdcheck: this.state.passwordcheck });
         if (result.data.server_responce == -1) {
           alert(
             "Either you are not a registered user or the email you entered is incorrect. Kindly contact us for more details."
@@ -243,12 +236,17 @@ export default class LoginScreen extends React.Component {
           console.log(result.data.server_responce_ID);
           active = result.data.server_responce_ID;
           if (active === "1") {
-            this.fadeOutLeft(); // animation
-            //  change from email to password
-            setTimeout(() => {
-              this.setState({ emailpage: false, loading: false });
-            }, 400);
-            this.scanFingerprint();
+            //  email verified success
+
+            // now check password
+            this.proceedFunctionPassword();
+
+            // this.fadeOutLeft(); // animation
+            // //  change from email to password
+            // setTimeout(() => {
+            //   this.setState({ emailpage: false, loading: false });
+            // }, 400);
+            // this.scanFingerprint();
           } else {
             alert("Your account is currently deactivated.");
             this.setState({ loading: false });
@@ -272,35 +270,20 @@ export default class LoginScreen extends React.Component {
   };
 
   proceedFunctionPassword = () => {
-    NetInfo.fetch().then((isConnected) => {
-      this.setState({ connections: !!isConnected });
-    });
-    if (this.state.connections) {
-      this.setState({ loading: true });
-      const { email, password } = this.state;
+    const { email, password } = this.state;
 
-      auth
-        .login({ email, password })
-        .then((user) => {
-          this.setState({ user });
-          // this.customToast.show("Successful sign in!");
-          console.log("successful sign in!");
-          this.setState({ loading: false });
-          setTimeout(() => {
-            this.props.navigation.navigate("AppDrawer");
-          }, 800);
-
-          // Idher Successful hochuka hay sign-in, Yahan se aagay navigate kardena main screen pe!
-        })
-        .catch((err) => {
-          alert("Error signing in. The password you entered might be incorrect. ");
-          console.log("error signing in!: ", err);
-          this.setState({ loading: false });
+    auth
+      .login({ email, password })
+      .then((user) => {
+        this.setState({ user, loading: false }, () => {
+          this.props.navigation.navigate("AppDrawer");
         });
-    } else {
-      alert("Internet connection failed!");
-      this.setState({ loading: false });
-    }
+      })
+      .catch((err) => {
+        alert("Error signing in. The password you entered might be incorrect. ");
+        console.log("error signing in!: ", err);
+        this.setState({ loading: false });
+      });
   };
 
   CallUsFunction = () => {
@@ -312,44 +295,19 @@ export default class LoginScreen extends React.Component {
     this.props.navigation.navigate("SignUpContectUs");
   };
 
-  // animation function
-  fadeOutLeft = () =>
-    this.view
-      ?.fadeOutLeftBig(400)
-      .then((endState) => console.log(endState.finished ? "bounce finished" : "bounce cancelled"));
+  onProceedButtonPress = () => {
+    if (this.state.email === "") {
+      return alert("Email cannot be empty");
+    }
+    if (this.state.password === "") {
+      return alert("Password cannot be empty");
+    }
 
-  fadeOutRight = () =>
-    this.view
-      ?.fadeOutRightBig(400)
-      .then((endState) => console.log(endState.finished ? "bounce finished" : "bounce cancelled"));
-
-  backtoEmail = () => {
-    this.fadeOutRight(); // animation
-
-    // change from password to email
-    setTimeout(() => {
-      this.setState({ emailpage: true });
-    }, 400);
+    this.proceedFunctionEmail();
   };
 
   ForgotPassword = () => {
-    this.setState({ loading: true });
-    const { email } = this.state;
-    Auth.forgotPassword(email)
-      .then((data) => {
-        console.log(data);
-        this.setState({ loading: false });
-        this.props.navigation.navigate("ForgotPassword", {
-          UserEmail: this.state.email,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        this.setState({ loading: false });
-        alert(
-          "Either you are not a registered user or the email you entered is incorrect. Kindly contact us for more details."
-        );
-      });
+    return console.log("Forgot password will not work, its old code");
   };
 
   contactEmailHandler = () => {
@@ -358,29 +316,15 @@ export default class LoginScreen extends React.Component {
 
   render() {
     return (
-      // content as view type  and touch exit
-      // <Content scrollEnabled={false} style={{ flex:1}}>
       <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-        <Toast
-          ref={this.customToast}
-          textStyle={{
-            color: "#fff",
-          }}
-          style={{
-            backgroundColor: "#FFCA5D",
-          }}
-        />
-
         {/* background gradinet   */}
         <LinearGradient colors={["#000E1E", "#001E2B", "#000E1E"]} style={styles.gradiantStyle} />
 
         <Image style={styles.LogoStyle} source={require("../../assets/Login/Queensman_logo3.png")} />
-        <View style={{ paddingTop: "11%" }} />
+        {/* <View style={{ paddingTop: "11%" }} /> */}
 
-        {/* checking if to change email and password Component */}
-
-        {this.state.emailpage ? (
-          <Animatable.View style={{ width: "100%" }} ref={this.handleViewRef}>
+        <View>
+          <View style={{ width: "100%" }} ref={this.handleViewRef}>
             <ImageBackground
               style={{
                 height: 43,
@@ -393,8 +337,8 @@ export default class LoginScreen extends React.Component {
               <View style={{ justifyContent: "center", paddingLeft: "15%" }}>
                 <TextInput
                   keyboardType="email-address"
-                  ref={this.textInputMobile}
                   style={{ fontSize: 15, fontFamily: "Helvetica" }}
+                  value={this.state.email}
                   placeholder="Email"
                   autoCapitalize="none"
                   underlineColorAndroid="transparent"
@@ -405,117 +349,96 @@ export default class LoginScreen extends React.Component {
               </View>
             </ImageBackground>
 
-            <View style={{ paddingVertical: "3%", width: 280 }} />
+            <View style={{ width: "100%", alignItems: "center", marginTop: "8%" }}>
+              <ImageBackground
+                style={{ width: 280, height: 43 }}
+                source={require("../../assets/Login/Password_field4.png")}
+              >
+                <View
+                  style={{
+                    justifyContent: "center",
+                    paddingTop: "2%",
+                    paddingLeft: "15%",
+                    flexDirection: "row",
+                  }}
+                >
+                  <TextInput
+                    style={{
+                      fontSize: 15,
+                      fontFamily: "Helvetica",
+                      width: "80%",
+                    }}
+                    value={this.state.password}
+                    placeholder="Password"
+                    autoCapitalize="none"
+                    secureTextEntry={this.state.showPassword}
+                    underlineColorAndroid="transparent"
+                    onChangeText={(password) => {
+                      this.setState({ password });
+                    }} // password set
+                  />
+                  {this.state.showPassword ? (
+                    <Icon
+                      onPress={this.toggleSwitch}
+                      name="eye-off"
+                      style={{
+                        paddingTop: "2%",
+                        fontSize: 25,
+                        color: "#000E1E",
+                        paddingRight: "4%",
+                      }}
+                    />
+                  ) : (
+                    <Icon
+                      onPress={this.toggleSwitch}
+                      name="eye"
+                      style={{
+                        paddingTop: "2%",
+                        fontSize: 25,
+                        color: "#000E1E",
+                        paddingRight: "4%",
+                      }}
+                    />
+                  )}
+                </View>
+              </ImageBackground>
+            </View>
+
+            <View style={{ paddingVertical: "5%", width: 280 }} />
             <TouchableOpacity
               style={{ height: 38, width: 200, alignSelf: "center" }}
-              onPress={this.proceedFunctionEmail}
+              onPress={() => {
+                this.onProceedButtonPress();
+              }}
             >
-              <ImageBackground style={{ height: 38, width: 200 }} source={require("../../assets/Login/Proceed2.png")} />
+              {this.state.loading ? (
+                <ActivityIndicator size="small" color="#fff" style={{ paddingTop: "5%" }} />
+              ) : (
+                <ImageBackground
+                  style={{ height: 38, width: 200 }}
+                  source={require("../../assets/Login/Proceed2.png")}
+                />
+              )}
             </TouchableOpacity>
 
             <TouchableOpacity style={{ alignSelf: "center", paddingTop: "5%" }} onPress={this.ForgotPassword}>
               <Text style={{ fontSize: 15, fontFamily: "Helvetica", color: "#fff" }}>Forgot Password?</Text>
             </TouchableOpacity>
-          </Animatable.View>
-        ) : (
-          <Animatable.View
-            style={{ width: "100%", alignItems: "center" }}
-            animation="fadeInRightBig"
-            iterationCount={1}
-            ref={this.handleViewRefs}
-          >
-            <ImageBackground
-              style={{ width: 280, height: 43 }}
-              source={require("../../assets/Login/Password_field4.png")}
-            >
-              <View
-                style={{
-                  justifyContent: "center",
-                  paddingTop: "2%",
-                  paddingLeft: "15%",
-                  flexDirection: "row",
-                }}
-              >
-                <TextInput
-                  ref={this.textInputMobile}
-                  style={{
-                    fontSize: 15,
-                    fontFamily: "Helvetica",
-                    width: "80%",
-                  }}
-                  placeholder="Password"
-                  autoCapitalize="none"
-                  secureTextEntry={this.state.showPassword}
-                  underlineColorAndroid="transparent"
-                  onChangeText={(password) => {
-                    this.setState({ password });
-                  }} // password set
-                />
-                {this.state.showPassword ? (
-                  <Icon
-                    onPress={this.toggleSwitch}
-                    name="eye-off"
-                    style={{
-                      paddingTop: "2%",
-                      fontSize: 25,
-                      color: "#000E1E",
-                      paddingRight: "4%",
-                    }}
-                  />
-                ) : (
-                  <Icon
-                    onPress={this.toggleSwitch}
-                    name="eye"
-                    style={{
-                      paddingTop: "2%",
-                      fontSize: 25,
-                      color: "#000E1E",
-                      paddingRight: "4%",
-                    }}
-                  />
-                )}
-              </View>
-            </ImageBackground>
+          </View>
 
-            <View style={{ paddingVertical: "3%" }} />
-            <TouchableOpacity style={{ width: 200, height: 38 }} onPress={this.proceedFunctionPassword}>
-              <ImageBackground style={{ width: 200, height: 38 }} source={require("../../assets/Login/Proceed2.png")} />
-            </TouchableOpacity>
+          <View
+            style={{
+              borderBottomColor: "#FFCA5D",
+              borderBottomWidth: 1,
+              width: 280,
+              paddingTop: "17%",
+              alignSelf: "center",
+              justifyContent: "center",
+            }}
+          />
 
-            <Text
-              style={{
-                fontSize: 10,
-                fontFamily: "Helvetica",
-                color: "#fff",
-                alignSelf: "center",
-                paddingTop: "3%",
-              }}
-            >
-              You may also use your phone fingerprint to login
-            </Text>
-            {/* go back to email */}
-            <TouchableOpacity style={{ alignSelf: "center", paddingTop: "5%" }} onPress={this.backtoEmail}>
-              <Text style={{ fontSize: 15, fontFamily: "Helvetica", color: "#fff" }}>Back to email</Text>
-            </TouchableOpacity>
-          </Animatable.View>
-        )}
-
-        {this.state.loading ? <ActivityIndicator size="large" color="#fff" style={{ paddingTop: "5%" }} /> : <View />}
-
-        {/* call us */}
-
-        <View
-          style={{
-            borderBottomColor: "#FFCA5D",
-            borderBottomWidth: 1,
-            width: 280,
-            paddingTop: "17%",
-            alignSelf: "center",
-          }}
-        />
-        {this.state.emailpage ? (
           <TouchableOpacity onPress={this.ContectUsFuntion}>
-            <View style={{ flexDirection: "column", paddingTop: "5%" }}>
+            <View style={{ flexDirection: "column", paddingTop: "5%", alignSelf: "center" }}>
               <Text style={{ fontSize: 11, fontFamily: "Helvetica", color: "#fff" }}>Haven't registered yet?</Text>
 
               <Text
@@ -530,61 +453,29 @@ export default class LoginScreen extends React.Component {
               </Text>
             </View>
           </TouchableOpacity>
-        ) : (
-          <TouchableOpacity onPress={this.CallUsFunction}>
-            <View style={{ flexDirection: "column", paddingTop: "5%" }}>
+
+          <TouchableOpacity style={{ marginTop: "3%" }} onPress={this.contactEmailHandler}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignSelf: "center",
+                alignItems: "center",
+              }}
+            >
+              <Text style={{ fontSize: 11, fontFamily: "Helvetica", color: "#fff" }}>Feel free to email us at </Text>
               <Text
                 style={{
                   fontSize: 11,
                   fontFamily: "Helvetica",
-                  color: "#fff",
-                  alignSelf: "center",
+                  color: "#FFCA5D",
                 }}
               >
-                Facing any issues?
+                services@queensman.com
               </Text>
-              <View style={{ flexDirection: "row" }}>
-                <Image
-                  style={{ height: 18, width: 18, marginTop: "3%" }}
-                  source={require("../../assets/Login/Phone.png")}
-                />
-                <Text> </Text>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    fontFamily: "Helvetica",
-                    color: "#FFCA5D",
-                  }}
-                >
-                  CALL US NOW
-                </Text>
-              </View>
             </View>
           </TouchableOpacity>
-        )}
-        <View style={{ height: "3%" }} />
-        <TouchableOpacity onPress={this.contactEmailHandler}>
-          <View
-            style={{
-              flexDirection: "row",
-              alignSelf: "center",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontSize: 11, fontFamily: "Helvetica", color: "#fff" }}>Feel free to email us at </Text>
-            <Text
-              style={{
-                fontSize: 11,
-                fontFamily: "Helvetica",
-                color: "#FFCA5D",
-              }}
-            >
-              services@queensman.com
-            </Text>
-          </View>
-        </TouchableOpacity>
+        </View>
       </KeyboardAvoidingView>
-      // </Content>
     );
   }
 }
