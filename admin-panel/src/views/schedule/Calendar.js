b654aaff78e295552c545f28a12696162e02d4a6
Mustafa Hanif <mustafa.hanif@seera.sa>
2021-04-04 15:07:19 +0400

    Add calendar

---
+++ admin-panel/src/views/schedule/Calendar.js
@@ -0,0 +1,187 @@
+// ** React Import
+import { useEffect, useRef, memo, Fragment } from 'react'
+
+// ** Full Calendar & it's Plugins
+import FullCalendar from '@fullcalendar/react'
+import listPlugin from '@fullcalendar/list'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+// ** Custom Components
+import Avatar from '@components/avatar'
+
+// ** Third Party Components
+import { toast } from 'react-toastify'
+import { Card, CardBody } from 'reactstrap'
+import { Menu, Check } from 'react-feather'
+
+// ** Toast Component
+const ToastComponent = ({ title, icon, color }) => (
+  <Fragment>
+    <div className='toastify-header pb-0'>
+      <div className='title-wrapper'>
+        <Avatar size='sm' color={color} icon={icon} />
+        <h6 className='toast-title'>{title}</h6>
+      </div>
+    </div>
+  </Fragment>
+)
+
+const Calendar = props => {
+  // ** Refs
+  const calendarRef = useRef(null)
+
+  // ** Props
+  const {
+    events,
+    isRtl,
+    dispatch,
+    calendarsColor,
+    calendarApi,
+    setCalendarApi,
+    handleAddEventSidebar,
+    blankEvent,
+    toggleSidebar,
+    selectEvent,
+    updateEvent
+  } = props
+
+  // ** UseEffect checks for CalendarAPI Update
+  useEffect(() => {
+    if (calendarApi === null) {
+      setCalendarApi(calendarRef.current.getApi())
+    }
+  }, [calendarApi])
+
+  // ** calendarOptions(Props)
+  const calendarOptions = {
+    events,
+    initialDate: '2020-08-01',
+    eventDataTransform: (eventData => {
+      const { id, worker: { full_name: workerName }, start, startTime, title } = eventData
+      return {
+        id,
+        title: `${title} by ${workerName}`,
+        start: `${start}T${startTime}`
+      }
+    }),
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    initialView: 'listMonth',
+    headerToolbar: {
+      start: 'sidebarToggle, prev,next, title',
+      end: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+    },
+    /*
+      Enable dragging and resizing event
+      ? Docs: https://fullcalendar.io/docs/editable
+    */
+    editable: false,
+
+    /*
+      Enable resizing event from start
+      ? Docs: https://fullcalendar.io/docs/eventResizableFromStart
+    */
+    eventResizableFromStart: false,
+
+    /*
+      Automatically scroll the scroll-containers during event drag-and-drop and date selecting
+      ? Docs: https://fullcalendar.io/docs/dragScroll
+    */
+    dragScroll: false,
+
+    /*
+      Max number of events within a given day
+      ? Docs: https://fullcalendar.io/docs/dayMaxEvents
+    */
+    dayMaxEvents: 10,
+
+    /*
+      Determines if day names and week names are clickable
+      ? Docs: https://fullcalendar.io/docs/navLinks
+    */
+    navLinks: true,
+
+    eventClassNames({ event: calendarEvent }) {
+      // eslint-disable-next-line no-underscore-dangle
+      const colorName = calendarsColor[calendarEvent._def.extendedProps.calendar]
+
+      return [
+        // Background Color
+        `bg-light-${colorName}`
+      ]
+    },
+
+    eventClick({ event: clickedEvent }) {
+      console.log(clickedEvent)
+      selectEvent(clickedEvent)
+      handleAddEventSidebar()
+
+      // * Only grab required field otherwise it goes in infinity loop
+      // ! Always grab all fields rendered by form (even if it get `undefined`) otherwise due to Vue3/Composition API you might get: "object is not extensible"
+      // event.value = grabEventDataFromEventApi(clickedEvent)
+
+      // eslint-disable-next-line no-use-before-define
+      // isAddNewEventSidebarActive.value = true
+    },
+
+    customButtons: {
+      sidebarToggle: {
+        text: <Menu className='d-xl-none d-block' />,
+        click() {
+          toggleSidebar(true)
+        }
+      }
+    },
+
+    dateClick(info) {
+      const ev = blankEvent
+      ev.start = info.date
+      ev.end = info.date
+      dispatch(selectEvent(ev))
+      handleAddEventSidebar()
+    },
+
+    /*
+      Handle event drop (Also include dragged event)
+      ? Docs: https://fullcalendar.io/docs/eventDrop
+      ? We can use `eventDragStop` but it doesn't return updated event so we have to use `eventDrop` which returns updated event
+    */
+    eventDrop({ event: droppedEvent }) {
+      dispatch(updateEvent(droppedEvent))
+      toast.success(<ToastComponent title='Event Updated' color='success' icon={<Check />} />, {
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeButton: false
+      })
+    },
+
+    /*
+      Handle event resize
+      ? Docs: https://fullcalendar.io/docs/eventResize
+    */
+    eventResize({ event: resizedEvent }) {
+      dispatch(updateEvent(resizedEvent))
+      toast.success(<ToastComponent title='Event Updated' color='success' icon={<Check />} />, {
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeButton: false
+      })
+    },
+
+    ref: calendarRef,
+
+    // Get direction from app state (store)
+    direction: isRtl ? 'rtl' : 'ltr'
+  }
+
+  return (
+    <Card className='shadow-none border-0 mb-0 rounded-0'>
+      <CardBody className='pb-0'>
+        <FullCalendar {...calendarOptions} />{' '}
+      </CardBody>
+    </Card>
+  )
+}
+
+export default memo(Calendar)
