d6fce86e2c21ed96a5763db7194b4121c2eb3bf9
hassan <m.h.raxa1@gmail.com>
2021-05-04 23:16:16 +0500

    Logout functionality added, notification screen done

--- Queensman-Client-master/src/Notification/index.js
+++ Queensman-Client-master/src/Notification/index.js
@@ -1,28 +1,45 @@
 import { LinearGradient } from "expo-linear-gradient";
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, ScrollView } from "react-native";
 import NotificationList from "./component/NotificationList";
+import { auth } from "../utils/nhost";
+import { gql, useQuery } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const GET_NOTIFICATIONS = gql`
+  query MyQuery($email: String!) {
+    client(where: { email: { _eq: $email } }) {
+      client_notifications {
+        notification
+      }
+    }
+  }
+`;
 
 export default function index() {
+  const email = auth?.currentSession?.session?.user.email;
+
+  console.log({ email });
+  const { loading, data, error } = useQuery(GET_NOTIFICATIONS, {
+    variables: { email },
+  });
+
+  console.log({ loading, data, error });
+  console.log("0000",data?.client[0].client_notifications)
   return (
     <>
       <LinearGradient colors={["#000E1E", "#001E2B", "#000E1E"]} style={styles.gradiantStyle} />
       <View style={{ flex: 1, paddingHorizontal: "5%", paddingVertical: "10%", marginTop: "15%" }}>
         <ScrollView>
-          <NotificationList
-            item={{
-              title: "test",
-              description: "test",
-              date: "22-May-2019",
-            }}
-          />
-          <NotificationList
-            item={{
-              title: "test",
-              description: "test",
-              date: "22-May-2019",
-            }}
-          />
+          {data?.client[0]?.client_notifications?.map((val) => (
+            <NotificationList
+              item={{
+                title: val.notification,
+                // description: "test",
+                // date: "22-May-2019",
+              }}
+            />
+          ))}
         </ScrollView>
       </View>
     </>
