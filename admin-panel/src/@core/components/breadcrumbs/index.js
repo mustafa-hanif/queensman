3c1a6b94e3b77fc76bdb9c17e23615021f3b1862
uzairifti <iftikhar_uzair@yahoo.com>
2021-06-07 12:10:42 +0500

    modal popup

--- admin-panel/src/@core/components/breadcrumbs/index.js
+++ admin-panel/src/@core/components/breadcrumbs/index.js
@@ -1,92 +1,219 @@
 // ** React Imports
-import { Link } from 'react-router-dom'
+import { Fragment, useState, useEffect } from 'react'
+
+// ** Invoice List Sidebar
+import Sidebar from './Sidebar'
+
+// ** Columns
+import { columns } from './columns'
+
+// ** Store & Actions
+import { useDispatch, useSelector } from 'react-redux'
 
 // ** Third Party Components
-import Proptypes from 'prop-types'
-import { Grid, CheckSquare, MessageSquare, Mail, Calendar } from 'react-feather'
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  UncontrolledButtonDropdown,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle
-} from 'reactstrap'
+import Select from 'react-select'
+import ReactPaginate from 'react-paginate'
+import { ChevronDown } from 'react-feather'
+import DataTable from 'react-data-table-component'
+import { selectThemeColors } from '@utils'
+import { Card, CardHeader, CardTitle, CardBody, Input, Row, Col, Label, CustomInput, Button } from 'reactstrap'
 
-const BreadCrumbs = props => {
-  // ** Props
-  const { breadCrumbTitle, breadCrumbParent, breadCrumbParent2, breadCrumbParent3, breadCrumbActive } = props
+// ** Styles
+import '@styles/react/libs/react-select/_react-select.scss'
+import '@styles/react/libs/tables/react-dataTable-component.scss'
 
+// ** Table Header
+const CustomHeader = ({ toggleSidebar, handlePerPage, rowsPerPage, handleFilter, searchTerm }) => {
   return (
-    <div className='content-header row'>
-      <div className='content-header-left col-md-9 col-12 mb-2'>
-        <div className='row breadcrumbs-top'>
-          <div className='col-12'>
-            {breadCrumbTitle ? <h2 className='content-header-title float-left mb-0'>{breadCrumbTitle}</h2> : ''}
-            <div className='breadcrumb-wrapper vs-breadcrumbs d-sm-block d-none col-12'>
-              <Breadcrumb>
-                <BreadcrumbItem tag='li'>
-                  <Link to='/'>Home</Link>
-                </BreadcrumbItem>
-                <BreadcrumbItem tag='li' className='text-primary'>
-                  {breadCrumbParent}
-                </BreadcrumbItem>
-                {breadCrumbParent2 ? (
-                  <BreadcrumbItem tag='li' className='text-primary'>
-                    {breadCrumbParent2}
-                  </BreadcrumbItem>
-                ) : (
-                  ''
-                )}
-                {breadCrumbParent3 ? (
-                  <BreadcrumbItem tag='li' className='text-primary'>
-                    {breadCrumbParent3}
-                  </BreadcrumbItem>
-                ) : (
-                  ''
-                )}
-                <BreadcrumbItem tag='li' active>
-                  {breadCrumbActive}
-                </BreadcrumbItem>
-              </Breadcrumb>
-            </div>
+    <div className='invoice-list-table-header w-100 mr-1 ml-50 mt-2 mb-75'>
+      <Row>
+        <Col xl='6' className='d-flex align-items-center p-0'>
+          <div className='d-flex align-items-center w-100'>
+            <Label for='rows-per-page'>Show</Label>
+            <CustomInput
+              className='form-control mx-50'
+              type='select'
+              id='rows-per-page'
+              value={rowsPerPage}
+              onChange={handlePerPage}
+              style={{
+                width: '5rem',
+                padding: '0 0.8rem',
+                backgroundPosition: 'calc(100% - 3px) 11px, calc(100% - 20px) 13px, 100% 0'
+              }}
+            >
+              <option value='10'>10</option>
+              <option value='25'>25</option>
+              <option value='50'>50</option>
+            </CustomInput>
+            <Label for='rows-per-page'>Entries</Label>
           </div>
-        </div>
-      </div>
-      <div className='content-header-right text-md-right col-md-3 col-12 d-md-block d-none'>
-        <div className='form-group breadcrum-right dropdown'>
-          <UncontrolledButtonDropdown>
-            <DropdownToggle color='primary' size='sm' className='btn-icon btn-round dropdown-toggle'>
-              <Grid size={14} />
-            </DropdownToggle>
-            <DropdownMenu tag='ul' right>
-              <DropdownItem tag={Link} to='/apps/chat'>
-                <CheckSquare className='mr-1' size={14} />
-                <span className='align-middle'>Todo</span>
-              </DropdownItem>
-              <DropdownItem tag={Link} to='/apps/chat'>
-                <MessageSquare className='mr-1' size={14} />
-                <span className='align-middle'>Chat</span>
-              </DropdownItem>
-              <DropdownItem tag={Link} to='/apps/email'>
-                <Mail className='mr-1' size={14} />
-                <span className='align-middle'>Email</span>
-              </DropdownItem>
-              <DropdownItem tag={Link} to='/apps/calendar'>
-                <Calendar className='mr-1' size={14} />
-                <span className='align-middle'>Calendar</span>
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledButtonDropdown>
-        </div>
-      </div>
+        </Col>
+        <Col
+          xl='6'
+          className='d-flex align-items-sm-center justify-content-lg-end justify-content-start flex-lg-nowrap flex-wrap flex-sm-row flex-column pr-lg-1 p-0 mt-lg-0 mt-1'
+        >
+          <div className='d-flex align-items-center mb-sm-0 mb-1 mr-1'>
+            <Label className='mb-0' for='search-invoice'>
+              Search:
+            </Label>
+            <Input
+              id='search-invoice'
+              className='ml-50 w-100'
+              type='text'
+              value={searchTerm}
+              onChange={e => handleFilter(e.target.value)}
+            />
+          </div>
+          <Button.Ripple color='primary' onClick={toggleSidebar}>
+            Add New User
+          </Button.Ripple>
+        </Col>
+      </Row>
     </div>
   )
 }
-export default BreadCrumbs
 
-// ** PropTypes
-BreadCrumbs.propTypes = {
-  breadCrumbTitle: Proptypes.string.isRequired,
-  breadCrumbActive: Proptypes.string.isRequired
+const UsersList = () => {
+  // ** Store Vars
+  const dispatch = useDispatch()
+  const store = useSelector(state => state.users)
+
+  // ** States
+  const [searchTerm, setSearchTerm] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [currentRole, setCurrentRole] = useState({ value: '', label: 'Select Role' })
+  const [currentPlan, setCurrentPlan] = useState({ value: '', label: 'Select Plan' })
+  const [currentStatus, setCurrentStatus] = useState({ value: '', label: 'Select Status', number: 0 })
+
+  // ** Function to toggle sidebar
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen)
+
+  // ** User filter options
+  const roleOptions = [
+    { value: '', label: 'Select Role' },
+    { value: 'admin', label: 'Admin' },
+    { value: 'author', label: 'Author' },
+    { value: 'editor', label: 'Editor' },
+    { value: 'maintainer', label: 'Maintainer' },
+    { value: 'subscriber', label: 'Subscriber' }
+  ]
+
+  const planOptions = [
+    { value: '', label: 'Select Plan' },
+    { value: 'basic', label: 'Basic' },
+    { value: 'company', label: 'Company' },
+    { value: 'enterprise', label: 'Enterprise' },
+    { value: 'team', label: 'Team' }
+  ]
+
+  const statusOptions = [
+    { value: '', label: 'Select Status', number: 0 },
+    { value: 'pending', label: 'Pending', number: 1 },
+    { value: 'active', label: 'Active', number: 2 },
+    { value: 'inactive', label: 'Inactive', number: 3 }
+  ]
+
+  // ** Function in get data on page change
+  const handlePagination = page => {
+    dispatch(
+      getData({
+        page: page.selected + 1,
+        perPage: rowsPerPage,
+        role: currentRole.value,
+        currentPlan: currentPlan.value,
+        status: currentStatus.value,
+        q: searchTerm
+      })
+    )
+    setCurrentPage(page.selected + 1)
+  }
+
+  // ** Function in get data on rows per page
+  const handlePerPage = e => {
+    const value = parseInt(e.currentTarget.value)
+    dispatch(
+      getData({
+        page: currentPage,
+        perPage: value,
+        role: currentRole.value,
+        currentPlan: currentPlan.value,
+        status: currentStatus.value,
+        q: searchTerm
+      })
+    )
+    setRowsPerPage(value)
+  }
+
+  // ** Function in get data on search query change
+  const handleFilter = val => {
+    setSearchTerm(val)
+    dispatch(
+      getData({
+        page: currentPage,
+        perPage: rowsPerPage,
+        role: currentRole.value,
+        currentPlan: currentPlan.value,
+        status: currentStatus.value,
+        q: val
+      })
+    )
+  }
+
+  // ** Custom Pagination
+  const CustomPagination = () => {
+    const count = Number(Math.ceil(store.total / rowsPerPage))
+
+    return (
+      <ReactPaginate
+        previousLabel={''}
+        nextLabel={''}
+        pageCount={count || 1}
+        activeClassName='active'
+        forcePage={currentPage !== 0 ? currentPage - 1 : 0}
+        onPageChange={page => handlePagination(page)}
+        pageClassName={'page-item'}
+        nextLinkClassName={'page-link'}
+        nextClassName={'page-item next'}
+        previousClassName={'page-item prev'}
+        previousLinkClassName={'page-link'}
+        pageLinkClassName={'page-link'}
+        containerClassName={'pagination react-paginate justify-content-end my-2 pr-1'}
+      />
+    )
+  }
+
+  return (
+    <Fragment>
+     
+      <Card>
+        <DataTable
+          noHeader
+          pagination
+          subHeader
+          responsive
+          paginationServer
+          columns={columns}
+          sortIcon={<ChevronDown />}
+          className='react-dataTable'
+          paginationComponent={CustomPagination}
+          subHeaderComponent={
+            <CustomHeader
+              toggleSidebar={toggleSidebar}
+              handlePerPage={handlePerPage}
+              rowsPerPage={rowsPerPage}
+              searchTerm={searchTerm}
+              handleFilter={handleFilter}
+            />
+          }
+        />
+      </Card>
+
+      <Sidebar open={sidebarOpen} toggleSidebar={toggleSidebar} />
+    </Fragment>
+  )
 }
+
+export default UsersList
