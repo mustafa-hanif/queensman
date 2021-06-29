3b9ccd5a74ea3ae431634405efe4afecb1824469
Mustafa Hanif <mustafa.hanif@seera.sa>
2021-06-29 13:30:26 +0400

    resolve conflict

--- admin-panel/src/views/clients/index.js
+++ admin-panel/src/views/clients/index.js
@@ -15,35 +15,6 @@ import TableZeroConfig from './TableZeroConfig'
 
 // ** Styles
 import '@styles/react/libs/tables/react-dataTable-component.scss'
-<<<<<<< HEAD
-=======
-import { gql, useLazyQuery, useQuery } from "@apollo/client"
-
-const GET_CLIENT = gql`
-query GetClient {
-  client {
-    email
-    full_name
-    password
-  phone
-  account_type
-  active
-    
-  }
-}
-`
-function Clients() {
-  const [isOpen, setIsOpen] = useState(false)
- 
-  const togglePopup = () => {
-    setIsOpen(!isOpen)
-  }
- 
-  const { loading, error, data } = useQuery(GET_CLIENT)
-
-  if (loading) return 'Loading...'
-  if (error) return `Error!-+ ${error.message}`
->>>>>>> d06259588d5957af35786c146399aef3a1a3efb9
 
 const Tables = () => {
   return (
