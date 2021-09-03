0278bc0632f0ca8b869631acfb85733cd324b92e
Salman Hanif <salmanhanif133@gmail.com>
2021-09-03 16:06:08 +0500

    Analytics 0.1

--- admin-panel/src/views/Home.js
+++ admin-panel/src/views/Home.js
@@ -1,52 +1,41 @@
+import { Fragment, useContext } from 'react'
 import { Row, Col } from 'reactstrap'
-
+import { kFormatter } from '@utils'
+import { ThemeColors } from '@src/utility/context/ThemeColors'
+import OrdersReceived from './OrdersReceived'
+import SupportTracker from './SupportTracker'
+import SubscribersGained from './SubscribersGained'
+import BarChart from './BarChart'
 import StatsCard from '@src/views/ui-elements/cards/statistics/StatsCard'
+import { useRTL } from '@hooks/useRTL'
 import '@styles/react/libs/charts/apex-charts.scss'
 import '@styles/base/pages/dashboard-ecommerce.scss'
+
+
 const Home = () => {
+  const { colors } = useContext(ThemeColors)
+  const [isRtl, setIsRtl] = useRTL()
   return (
-    <div id='dashboard-ecommerce'>
+    <Fragment>
       <Row className='match-height'>
         <Col xl='12' md='12' xs='12'>
           <StatsCard cols={{ xl: '3', sm: '6' }} />
         </Col>
       </Row>
       <Row className='match-height'>
-        <Col lg='4' md='12'>
-          <Row className='match-height'>
-            <Col lg='6' md='3' xs='6'>
-              {/* <OrdersBarChart warning={colors.warning.main} /> */}
-            </Col>
-            <Col lg='6' md='3' xs='6'>
-              {/* <ProfitLineChart info={colors.info.main} /> */}
-            </Col>
-            <Col lg='12' md='6' xs='12'>
-              {/* <Earnings success={colors.success.main} /> */}
-            </Col>
-          </Row>
+        <Col lg='12' sm='6'>
+          <BarChart direction={isRtl ? 'rtl' : 'ltr'} info={colors.info.main} />
         </Col>
-        <Col lg='8' md='12'>
-          {/* <RevenueReport primary={colors.primary.main} warning={colors.warning.main} /> */}
+        {/* <Col lg='3' sm='6'>
+          <OrdersReceived kFormatter={kFormatter} warning={colors.warning.main} />
+        </Col> */}
+      </Row>
+      <Row>
+        <Col lg='6' xs='12'>
+          <SupportTracker primary={colors.primary.main} danger={colors.danger.main} />
         </Col>
       </Row>
-      <Row className='match-height'>
-        <Col lg='8' xs='12'>
-          {/* <CompanyTable /> */}
-        </Col>
-        <Col lg='4' md='6' xs='12'>
-          {/* <CardMeetup /> */}
-        </Col>
-        <Col lg='4' md='6' xs='12'>
-          {/* <CardBrowserStates colors={colors} trackBgColor={trackBgColor} /> */}
-        </Col>
-        <Col lg='4' md='6' xs='12'>
-          {/* <GoalOverview success={colors.success.main} /> */}
-        </Col>
-        <Col lg='4' md='6' xs='12'>
-          {/* <CardTransactions /> */}
-        </Col>
-      </Row>
-    </div>
+    </Fragment>
   )
 }
 
---
+++ admin-panel/src/views/SubscribersGained.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Users } from 'react-feather'
+import StatsWithAreaChart from '@components/widgets/stats/StatsWithAreaChart'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_CALLOUT_COUNT_TODAY = gql`
+query CalloutsToday($date: timestamptz!) {
+    callout_aggregate(where: {request_time: {_eq: $date}}) {
+      aggregate {
+        count
+      }
+    }
+  }
+`
+
+const SubscribersGained = ({ kFormatter }) => {
+  const addHours = (date, hours) => {
+    return new Date(new Date(date).setHours(new Date(date).getHours() + hours)).toISOString()
+  }
+
+  const { loading, data, error } = useQuery(GET_CALLOUT_COUNT_TODAY, {variables: {date: Date()}})
+  if (!loading) {
+    const modifiedData = data?.callout_aggregate.aggregate.count
+    console.log(modifiedData)
+  } 
+  const [data2, setData] = useState({
+    series: [
+      {
+        name: 'Callouts',
+        data: [28, 40, 36, 52, 38, 60, 55]
+      }
+    ],
+    analyticsData: {
+      subscribers: 92600
+    }
+})
+
+  return data2 !== null ? (
+    <StatsWithAreaChart
+      icon={<Users size={21} />}
+      color='primary'
+      stats={kFormatter(data2.analyticsData.subscribers)}
+      statTitle='New Callouts'
+      series={data2.series}
+      type='area'
+    />
+  ) : null
+}
+
+export default SubscribersGained
