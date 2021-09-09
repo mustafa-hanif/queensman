1660d0abf53b37f287b0e104767913ca7a52b765
Salman Hanif <salmanhanif133@gmail.com>
2021-09-09 18:36:57 +0500

    serveless

--- queensman-api/api/sendInventoryClientEmail.js
+++ queensman-api/api/sendInventoryClientEmail.js
@@ -8,28 +8,7 @@ const sendInventoryClientEmail = async (event) => {
   console.log(event.body)
   const inventory_report_pdf = query.inventory_report_pdf;
   const property = query.property
-  // const client = query.client
-  const client = [{
-    id: 6,
-    email: 'salmanhanif133@gmail.com',
-    full_name: 'Salman Hanif',
-    account_type: 'Investor',
-    __typename: 'client'
-  },
-  {
-    id: 7,
-    email: 'murtaza.hanif@techinoviq.com',
-    full_name: 'Murtaza Hanif',
-    account_type: 'Investor',
-    __typename: 'client'
-  },
-  {
-    id: 8,
-    email: 'murtaza.hanif@gmail.com',
-    full_name: 'Murtaza Hanif',
-    account_type: 'Investor',
-    __typename: 'client'
-  }]
+  const client = query.client
 
   for (let i = 0; i < client.length; i++) {
     await inventoryClientEmail(inventory_report_pdf, property, client[i])    
