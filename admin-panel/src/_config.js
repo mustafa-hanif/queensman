3e81cad89fb6d71c6a623d40b477642cc098a080
Salman Hanif <salmanhanif133@gmail.com>
2021-09-26 22:49:58 +0500

    dev

--- admin-panel/src/_config.js
+++ admin-panel/src/_config.js
@@ -12,8 +12,8 @@
 
 module.exports = {
     DOMAIN: "https://meqmfvxx0d.execute-api.us-east-1.amazonaws.com/production",
-    HASURA: "https://backend-8106d23e.nhost.app",
-    ENDPOINT: "https://hasura-8106d23e.nhost.app/v1/graphql"
+    HASURA: "https://backend-cf57bf4d.nhost.app",
+    ENDPOINT: "https://hasura-cf57bf4d.nhost.app/v1/graphql"
     //  DOMAIN: "https://meqmfvxx0d.execute-api.us-east-1.amazonaws.com/production"
     // Change domain to above url for produciton dev
 }
